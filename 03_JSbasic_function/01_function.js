12f4ace0b2f15433b830a3b23d451d176226facb
Suaib007 <suaibsaifi07@gmail.com>
2024-08-20 15:08:23 +0530

    Array function

--- 03_JSbasic_function/01_function.js
+++ 03_JSbasic_function/01_function.js
@@ -22,7 +22,7 @@ function addTwoNumbers(num1, num2) {
 const result = addTwoNumbers(3, 5);
 // console.log("Result: ", result);
 
-function loginUserMessage(username) {
+function loginUserMessage(username = "sam") {
   if (username === undefined) {
     console.log("please enter a username");
     return;
@@ -31,4 +31,35 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("Shoaib"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Mr.Blacksmith"));
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 300, 100));
+
+const user = {
+  username: "Shoaib",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price} `
+  );
+}
+// handleObject(user);
+handleObject({
+  username: "sam",
+  price: 2000,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 4000, 100, 600]));
