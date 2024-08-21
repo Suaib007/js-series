7ca0f9508b4b3f8f59d4fd8fe2226f1c26bd22e3
Suaib007 <suaibsaifi07@gmail.com>
2024-08-21 12:55:29 +0530

    Array.reduce method

---
+++ 05_iteration/eight.js
@@ -0,0 +1,34 @@
+const myNumbers = [1, 2, 3, 4];
+
+// const myTotal = myNumbers.reduce(function (acc, currval) {
+//   console.log(`acc: ${acc} and currval: ${currval}`);
+
+//   return acc + currval;
+// }, 0);
+
+const myTotal = myNumbers.reduce((acc, currval) => acc + currval, 0);
+
+console.log(myTotal);
+
+const shoppongCart = [
+  {
+    itemName: "js course",
+    price: 299,
+  },
+  {
+    itemName: "py course",
+    price: 2000,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 5999,
+  },
+  {
+    itemName: "dsa course",
+    price: 12999,
+  },
+];
+
+const priceToPay = shoppongCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(priceToPay);
