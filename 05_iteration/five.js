0fe7c2293444095fde643db021ad8bbbe5d851fb
Suaib007 <suaibsaifi07@gmail.com>
2024-08-21 12:01:14 +0530

    forEach loop in Array/Object

---
+++ 05_iteration/five.js
@@ -0,0 +1,42 @@
+const coding = ["cpp", "ruby", "js", "tailwind", "java"];
+
+// coding.forEach(function (item) {
+//   console.log(item);
+// });
+
+// coding.forEach((item) => {
+//   console.log(item);
+// });
+
+// function printMe(item) {
+//   console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+const myCoding = [
+  {
+    languageName: "Javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+  {
+    languageName: "Java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "html",
+    languageFileName: "html",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item.languageName, item.languageFileName);
+});
